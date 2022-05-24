22667304754205c2256e92a3d1b9edf27f61a7ab
shangling <expixyz@163.com>
2022-05-24 17:49:12 +0800

    Dup1

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
-// Echo2 prints its command-like arguments.
+// Dup1 prints the text of each lin that appears more than
+// once in the standard input, preceded by its count.
+
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
